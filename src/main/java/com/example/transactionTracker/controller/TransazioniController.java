db811dc7b62fa262536cde0a4a8991b82a91c6cd
MatteoCovelli <covelli.matteo@icloud.com>
2024-09-10 21:18:10 +0200

    aggiunto addTransazioniFromFile + modifiche minori

--- src/main/java/com/example/transactionTracker/controller/TransazioniController.java
+++ src/main/java/com/example/transactionTracker/controller/TransazioniController.java
@@ -4,7 +4,6 @@ package com.example.transactionTracker.controller;
 import com.example.transactionTracker.model.Transazione;
 
 import com.example.transactionTracker.service.TransazioniServiceImpl;
-import com.example.transactionTracker.utils.Calcoli;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +12,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Controller
@@ -40,11 +41,33 @@ public class TransazioniController {
     }
 
     @PostMapping("/aggiungiTransazioni")
-    public String addTransazioni(@RequestParam String strTransazioni, Authentication authentication){
+    public String addTransazioni(@RequestParam String strTransazioni, Authentication authentication) {
         log.info(strTransazioni);
         service.addTransazioni(strTransazioni, authentication.getName());
 
         return "home";
     }
+
+    @PostMapping("/addTransazioniFromFile")
+    public String addTransazioniFromFile(@RequestParam("file") MultipartFile file, Authentication authentication) {
+
+        try {
+            String fileName = file.getOriginalFilename();
+            String extension = fileName.substring(fileName.length() - 4);
+
+            if (!file.isEmpty() && ".txt".equals(extension)) {
+                String content = new String(file.getBytes(), StandardCharsets.UTF_8);
+                service.addTransazioni(content, authentication.getName());
+            } else {
+                log.info("ESTENSIONE " + extension);
+                log.error("ERRORE: Il file caricato è vuoto oppure non è del formato corretto");
+            }
+        } catch (Exception e) {
+            log.error("ERRORE UPLOAD FILE");
+        }
+
+        return "home";
+    }
+
 
 }
