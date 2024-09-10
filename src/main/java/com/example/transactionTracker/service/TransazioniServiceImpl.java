cb20e78190f6b6d1202addccdba8fbcda3772e67
MatteoCovelli <covelli.matteo@icloud.com>
2024-09-10 21:27:36 +0200

    fix inserimento transazioni

--- src/main/java/com/example/transactionTracker/service/TransazioniServiceImpl.java
+++ src/main/java/com/example/transactionTracker/service/TransazioniServiceImpl.java
@@ -3,17 +3,12 @@ package com.example.transactionTracker.service;
 import com.example.transactionTracker.model.Transazione;
 import com.example.transactionTracker.repository.TransazioniRepository;
 import com.example.transactionTracker.utils.Calcoli;
-import org.apache.commons.logging.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 @Service
@@ -39,20 +34,11 @@ public class TransazioniServiceImpl implements TransazioniService {
     public void addTransazioni(String transazioni, String nomeUtente) {
 
         try {
-
             Calcoli c = Calcoli.builder().build();
-
             List<String> lista = Arrays.asList(transazioni.split("\n"));
 
-
-
-
-
-
             for (String el : lista) {
-
                 Transazione transazione = Transazione.builder().build();
-
 
                 if (el.contains("PAGAMENTO POS")) {
                     log.info("PAGAMENTO POS");
@@ -60,18 +46,18 @@ public class TransazioniServiceImpl implements TransazioniService {
                 } else if (el.contains("STIPENDIO/PENSIONE")) {
                     log.info("STIPENDIO/PENSIONE");
 
-
                 }
 
+                if (transazione.getNomeTransazione() != null) {
+                    log.info("SALVO TRANSAZIONE");
+                    repository.save(transazione);
+                }
 
-                log.info("SALVO TRANSAZIONE");
-                repository.save(transazione);
-            }//fine for
+            } //fine for
 
         } catch (Exception exception) {
             log.error("ERRORE: Formato dati in input non corretto");
         }
-
     }
 
 }
